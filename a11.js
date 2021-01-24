4f514615ef52249a6aad538853ef9d13ef31aa0f
hixsi <77935852+hixsi@users.noreply.github.com>
2021-01-24 20:58:44 +0200

    Add files via upload

---
+++ a11.js
@@ -0,0 +1,22 @@
+Modern art includes artistic work produced during the period extending 
+roughly from the 1860s to the 1970s, and denotes the styles and philosophies 
+of the art produced during that era.[1] The term is usually associated with 
+art in which the traditions of the past have been thrown aside in a spirit of 
+experimentation.[2] Modern artists experimented with new ways of seeing and 
+with fresh ideas about the nature of materials and functions of art. 
+A tendency away from the narrative, which was characteristic for the traditional arts,
+ toward abstraction is characteristic of much modern art. More recent artistic production 
+ is often called contemporary art or postmodern art.
+
+Modern art begins with the heritage of painters like Vincent van Gogh,
+ Paul Cézanne, Paul Gauguin, Georges Seurat and Henri de Toulouse-Lautrec
+ all of whom were essential for the development of modern art. 
+ At the beginning of the 20th century Henri Matisse and several other young artists 
+ including the pre-cubists Georges Braque, André Derain, Raoul Dufy, Jean Metzinger 
+ and Maurice de Vlaminck revolutionized the Paris art world with "wild", multi-colored,
+ expressive landscapes and figure paintings that the critics called Fauvism. Matisse's 
+ two versions of The Dance signified a key point in his career and in the development of 
+ modern painting.[3] It reflected Matisse's incipient fascination with primitive art: 
+ the intense warm color of the figures against the cool blue-green 
+background and the rhythmical succession of the dancing nudes convey the feelings of
+ emotional liberation and hedonism. 
